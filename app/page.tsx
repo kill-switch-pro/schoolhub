419989f61e15466b899f540919b5a9bc3e73f4f8
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2023-12-29 19:40:22 +0100

    chnaged login style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import LoginPage from "./login/page";
+//import ".../globals.css";
+//import "@/app/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
 
+import "./login/signuppage/signuppage.css";
+import "./globals.css";
+import { Envelope, Key, Lock, Person } from "react-bootstrap-icons";
+//import Image from "next/image";
+import "./login/signuppage/signuppage.css";
 export default function LandingPage() {
   return (
     <>
