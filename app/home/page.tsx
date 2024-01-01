46e69f600a82689d1e6aa04e5eaccb951db1b3fe
STEPHEN SAMUEL URAH <STEPHENSAMUEL063@GMAIL.COM>
2024-01-01 02:32:10 +0100

    modified taillwind css

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import "./home.css";
+import "../globals.css";
+//import "./home.css";
 import {
   Chat,
   Gear,
   Google,
   House,
   Lightbulb,
+  Magnet,
   Search,
 } from "react-bootstrap-icons";
 import { time } from "console";
 import { title } from "process";
 import Link from "next/link";
+//import "bootstrap/dist/css/bootstrap.css";
 
 const Navigation = [
   {
@@ -38,6 +41,9 @@ function Home() {
         <div className="max-w-screen-lg w-full h-full flex relative">
           {/*left side bar for navigation*/}
           <section className="fixed w-72 flex flex-col h-screen">
+            <Link href={"/"} className="my-4">
+              <Magnet size={25} />
+            </Link>
             {Navigation.map((item) => (
               <Link
                 className="bg-white/50 flex items-center justify-center space-x-2 rounded-3xl p-4"
@@ -47,9 +53,12 @@ function Home() {
                 <div>
                   <item.icon size={30} />
                 </div>
-                <div>{item.title}</div>
+                {item.title !== "schoolhub" && <div>{item.title}</div>}
               </Link>
             ))}
+            <button className="rounded m-4 bg-red-700 p-4 text-center">
+              post
+            </button>
           </section>
         </div>
       </div>
